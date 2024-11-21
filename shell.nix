1c5b7baebfc5973042307ec617cb513b18fe9882
Mårten Lindblad <marten@zstream.se>
2024-11-21 15:31:04 +0100

    Add Nix shell

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ in
   pkgs.mkShell {
     packages = with pkgs; [
       python310
-      (poetry.override {python3 = python310;})
+      uv
       terraform
       google-cloud-sdk
     ];
@@ -13,10 +13,8 @@ in
       export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
         pkgs.stdenv.cc.cc
       ]}
-      export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
       export TERRAFORM_RUN_DIR=$(pwd)/infrastructure/terraform
       export PROJECT_ID="${settings.projectId}";
-      poetry install
       gcloud auth application-default set-quota-project $PROJECT_ID
       gcloud config set project $PROJECT_ID
     '';
